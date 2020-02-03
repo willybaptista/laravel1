5c258b59ffc1c408b88574f5c7e2688bb1896e08
Willy Baptista <willybaptista@hotmail.com>
2020-02-02 21:05:39 -0300

    Bitrix24

--- app/Http/Controllers/Bitrix24Controller.php
+++ app/Http/Controllers/Bitrix24Controller.php
@@ -46,7 +46,7 @@ class Bitrix24Controller extends Controller
             ], 422);
         }
 
-        $queryUrl = $this->webHook;
+        $queryUrl = $this->webHook();
 
         $queryData = http_build_query(array(
                         'fields' => array(    
